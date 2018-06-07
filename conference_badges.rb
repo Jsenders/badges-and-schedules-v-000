354ae69b0e2f269e0eed5b01e793d58da0c7ccdf
Joseph Senders <jasenders@gmail.com>
2018-06-07 18:12:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,9 @@ def badge_maker(name)
 "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-  list = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  list.each do |name|
-    puts "Hello, my name is #{name}."
-  end
+def batch_badge_creator(speakers)
+speakers = []
+
 end
 
 def assign_rooms(speakers)
