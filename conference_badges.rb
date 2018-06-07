1b14ca7f73dedae018456804168746cb79f9ae0f
Joseph Senders <jasenders@gmail.com>
2018-06-07 18:36:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(attendees)
     rooms
   end
 
-def printer
+def printer(attendees)
   batch_badge_creator(attendees).each { |badge| puts badge }
   assign_rooms(attendees).each { |room| puts room }
 end
