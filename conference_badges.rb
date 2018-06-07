7c554c9aa5605f461e5ec4fe46a47da5db682fde
Joseph Senders <jasenders@gmail.com>
2018-06-07 18:06:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 def badge_maker(name)
-puts "Hello, my name is #{name}."
+"Hello, my name is #{name}."
 end
+
 def batch_badge_creator
   list = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   list.each do |name|
