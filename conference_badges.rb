96461bb144d5e52f7e047e3e7eed0aa5baf2e502
Joseph Senders <jasenders@gmail.com>
2018-06-07 18:10:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,17 @@ def batch_badge_creator
     puts "Hello, my name is #{name}."
   end
 end
+
+def assign_rooms(speakers)
+  counter = 1
+    rooms = []
+    speakers.each do |name|
+      rooms << "Hello, #{name}! You'll be assigned to room #{counter}!"
+      counter += 1
+    end
+    rooms
+  end
+end  
+
+def printer
+end
